570528295ba02a2e8f5a54572ff894b480b3aa0e
Tabish Tanseef <tabishtanseef@gmail.com>
2018-10-31 15:24:04 +0530

    good

---
+++ Activity-1/js/data.js
@@ -0,0 +1,36 @@
+var heading = "Drag the words";
+var helpTxt = "Next from the words given below each sentence drag the noun and the adjective that describes it to the correct columns.";
+var activitTitle = "Drag the words.";
+var msgComplete = "End of Activity";
+var btnLabel = {
+	"submitbtn": "submit",
+	"nextbtn" : "SHOW Answer",
+	"tryAgain": "Try Again"
+}
+
+
+var dataCollection = [
+	"<span class='draggable'>Those</span> are the trees that Grandpa planted.",
+	"Kiran finished her homework early, but Liza did not do <span class='draggable'>hers</span>.",
+	"Hari  saw <span class='draggable'>himself</span> in the mirror.",
+	"<span class='draggable'>Which</span> is the shortest route to the temple?",
+	"<span class='draggable'>This</span> is my favourite teddy bear.",
+	"That bicycle is <span class='draggable'>mine</span>.",
+	"Taru and I  had to cook for <span class='draggable'>ourselves</span> since mom was late.",
+	"<span class='draggable'>That</span> is the restaurant.",
+	"<span class='draggable'>What</span> is the number of your house?",
+	"<span class='draggable'>This</span> is the best bakery  in town."
+]
+
+var softTh = ["himself"];
+ 
+var hardTh = ["hers","mine","ourselves"];
+
+var tTh=["Those","This","That","This"];
+
+var dTh=["Which","What"];
+
+
+
+
+
